f167ba3332d34d99f2130fb55e5e6772ccd3c220
Adarsh Sharma <adarshs861@gmail.com>
2024-12-20 16:49:10 +0530

    Vecitr

--- C++STL/Vectitr.cpp
+++ C++STL/Vectitr.cpp
@@ -1,7 +1,12 @@
-#include<iostream>
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
+    vector<int> vec = {2, 4, 6, 8, 0};
+
+    cout << "Vec.begin:" << *(vec.begin()) << endl;
+    cout << "Vec.end:" << *(vec.end()) << endl;
+
     return 0;
 }
